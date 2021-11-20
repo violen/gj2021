565e6265ad9fee401e9516ccbb1077931492a205
André Hauser <violen@users.noreply.github.com>
2021-11-20 01:22:24 +0100

    3rd Person experiments

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,32 +4,41 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public CharacterController cController;
     public float speed = 10.5f;
+    public float jumpForce = 3.5f;
     private float horizontalInput;
-    private float forwardInput;
+    private float verticalInput;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        // horizontalInput = Input.GetAxis("Horizontal");
-        // forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(verticalInput, 0f, horizontalInput).normalized;
 
-        // transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        // transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+        if (direction.magnitude >= 0.1f) {
+            float targetAngle = Mathf.Atan2(-direction.z, direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+
+
+            cController.Move(direction * speed * Time.deltaTime);
+        }
+
+        // perform a jump
+        if (Input.GetButtonDown("Jump")) {
+            // rBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
     }
 
     void FixedUpdate() {
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
-
-        transform.Translate(Vector3.forward * speed * forwardInput);
-        transform.Translate(Vector3.right * speed * horizontalInput);
+        
     }
 }
